7c4f5bb6f7614fbd6d7d4c41e43d5429f522f113
dduoerr <114564620+dduoerr@users.noreply.github.com>
2023-05-08 14:03:26 -0700

    Add files via upload
    successful Arduino codes

---
+++ irSensor.ino
@@ -0,0 +1,22 @@
+int ir_sensor_pin = 9; // IR sensor pin
+void setup()
+{
+  Serial.begin(9600); // Serial monitor at baud rate 9600
+  pinMode(ir_sensor_pin, INPUT); // Pin set as input
+}
+
+
+void loop() {
+    if (digitalRead(ir_sensor_pin)) // if Pin logic is HIGH
+    {
+      Serial.println("Object detected"); // display on Serial monitor when object detected 
+    }
+  
+    else {
+     Serial.println("Object not detected"); // display on Serial when object not detected 
+    }
+
+//  Serial.println(analogRead(ir_sensor_pin)); 
+//  Serial.println(digitalRead(ir_sensor_pin));
+//  delay(1000);
+}
